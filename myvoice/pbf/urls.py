9a8c3624bc0be57bea940a70f1a8dca0d2e4c7db
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-05-29 11:58:23 -0400

    Select which clinic to view dashboard for

--- myvoice/pbf/urls.py
+++ myvoice/pbf/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^dashboard/$', views.PBFDashboard.as_view(), name='pbf_dashboard'),
+    url(r'^dashboard/$',
+        views.PBFDashboardSelect.as_view(),
+        name='pbf_dashboard_select'),
+    url(r'^dashboard/(?P<clinic_slug>[\w+]+)/$',
+        views.PBFDashboard.as_view(),
+        name='pbf_dashboard'),
 ]
--- myvoice/pbf/views.py
+++ myvoice/pbf/views.py
@@ -1,4 +1,12 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+
+class PBFDashboardSelect(TemplateView):
+    template_name = 'pbf/select.html'
+
+    def post(self, request, *args, **kwargs):
+        return redirect('pbf_dashboard', 'wayo_matti')
 
 
 class PBFDashboard(TemplateView):
