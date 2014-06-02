dc84030e127a0c29899c55274b8a5fe65145226d
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-06-02 14:21:20 -0400

    Restructure to allow different clinic prototypes

---
+++ myvoice/pbf/models.py
@@ -0,0 +1,49 @@
+
+
+# Reference for clinic data until we set up a model structure.
+CLINIC_DATA = {
+    'arum-chugbu-phc': {
+        'name': 'Arum Chugbu PHC',
+        'slug': 'arum-chugbu-phc',
+    },
+    'gwagi-phc': {
+        'name': 'Gwagi PHC',
+        'slug': 'gwagi-phc',
+    },
+    'kwabe-phc': {
+        'name': 'Kwabe PHC',
+        'slug': 'kwabe-phc',
+    },
+    'kwarra-phc': {
+        'name': 'Kwarra PHC',
+        'slug': 'kwarra-phc',
+    },
+    'maraba-gongon-phc': {
+        'name': 'Maraba Gongon PHC',
+        'slug': 'maraba-gongon-phc',
+    },
+    'nakere-phc': {
+        'name': 'Nakere PHC',
+        'slug': 'nakere-phc',
+    },
+    'wamba-general-hospital': {
+        'name': 'Wamba General Hospital',
+        'slug': 'wamba-general-hospital',
+    },
+    'wayo-matti-phc': {
+        'name': 'Wayo Matti PHC',
+        'slug': 'wayo-matti-phc',
+    },
+    'wamba-phc-model-clinic': {
+        'name': 'Wamba PHC Model Clinic',
+        'slug': 'wamba-phc-model-clinic',
+    },
+    'yashi-madaki-phc': {
+        'name': 'Yashi Madaki PHC',
+        'slug': 'yashi-madaki-phc',
+    },
+    'zalli-phc': {
+        'name': 'Zalli PHC',
+        'slug': 'zalli-phc',
+    },
+}
--- myvoice/pbf/views.py
+++ myvoice/pbf/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.views.generic import FormView, TemplateView
+from django.http import Http404
+
+from .forms import SelectClinicForm
 
 
-class PBFDashboardSelect(TemplateView):
+class PBFDashboardSelect(FormView):
     template_name = 'pbf/select.html'
+    form_class = SelectClinicForm
 
-    def post(self, request, *args, **kwargs):
-        return redirect('pbf_dashboard', 'wayo-matti')
+    def form_valid(self, form):
+        return redirect('pbf_dashboard', form.cleaned_data['clinic'])
 
 
 class PBFDashboard(TemplateView):
     template_name = 'pbf/dashboard/dashboard.html'
+
+    def get(self, request, clinic_slug, *args, **kwargs):
+        from .models import CLINIC_DATA
+        if clinic_slug not in CLINIC_DATA:
+            raise Http404("Clinic with slug '{0}' not found".format(clinic_slug))
+        self.clinic = CLINIC_DATA.get(clinic_slug)
+        return super(PBFDashboard, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        kwargs['clinic'] = self.clinic
+        return super(PBFDashboard, self).get_context_data(**kwargs)
