7f31f030d82d878ef48252234ccb1b5c10d3a6f5
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-06-03 13:34:07 -0400

    Refactor performance progress

--- myvoice/pbf/models.py
+++ myvoice/pbf/models.py
@@ -33,25 +33,38 @@ CLINIC_DATA = {
     'wayo-matti-phc': {
         'name': 'Wayo Matti PHC',
         'slug': 'wayo-matti-phc',
+        'year_founded': '2011',
+        'show_feedback': False,
         'incharge': {
             'name': 'DPHC, Ezekiel Jagga',
             'years_at_clinic': '5',
             'contact_numbers': '0814 339 9384\n 0808 711 2929',
         },
-        'year_founded': '2011',
-        'show_feedback': False,
-        'performance_summary': [
-            ('Income', '722,652', 4),
-            ('Quality Score', '96.6%', 1),
-            ('Patient Satisfaction', '87%', 1),
-        ],
-        'services': [
-            ('New Out Patient', 'x', 1),
-            ('Normal Delivery', 'x', 1),
-            ('Child Immunization / Vaccination', 'x', 1),
-            ('Immunization or vaccination', 'x', 1),
-            ('Tests', 'x', 1),
-            ('Other\n(Including Antenatal Care)', 'x', 1),
+        'progress': [
+            ('Performance Summary', [
+                ('Income', '722,652', 4),
+                ('Quality Score', '96.6%', 1),
+                ('Patient Satisfaction', '87%', 1),
+            ]),
+            ('Patients', [
+                ('Total Patients seen', None, 4),
+                ('Outreach', 320, 1),
+                ('In Facility', None, 3),
+                ('Indigents', 35, 3),
+            ]),
+            ('Services', [
+                ('New Out Patient', 'x', 1),
+                ('Normal Delivery', 'x', 1),
+                ('Child Immunization / Vaccination', 'x', 1),
+                ('Immunization or vaccination', 'x', 1),
+                ('Tests', 'x', 1),
+                ('Other\n(Including Antenatal Care)', 'x', 1),
+            ]),
+            ('Feedback', [
+                ('Staff Treatment', '74%', 2),
+                ('Affordable Drugs', '89%', 3),
+                ('Avg. Wait Time', '1 hr.', 3),
+            ]),
         ],
     },
     'wamba-phc-model-clinic': {
@@ -72,18 +85,31 @@ CLINIC_DATA = {
         ],
         'total_positions': 26,
         'show_feedback': True,
-        'performance_summary': [
-            ('income', '938,238', 2),
-            ('Quality Score', '94.0%', 2),
-            ('Patient Satisfaction', '87%', 1),
-        ],
-        'services': [
-            ('New Out Patient', 'x', 1),
-            ('Normal Delivery', 'x', 1),
-            ('Child Immunization / Vaccination', 'x', 1),
-            ('Immunization or vaccination', 'x', 1),
-            ('Tests', 'x', 1),
-            ('Other\n(Including Antenatal Care)', 'x', 1),
+        'progress': [
+            ('Performance Summary', [
+                ('income', '938,238', 2),
+                ('Quality Score', '94.0%', 2),
+                ('Patient Satisfaction', '87%', 1),
+            ]),
+            ('Patients', [
+                ('Total Patients seen', None, 4),
+                ('Outreach', 320, 1),
+                ('In Facility', None, 3),
+                ('Indigents', 35, 3),
+            ]),
+            ('Services', [
+                ('New Out Patient', None, None),
+                ('Normal Delivery', None, None),
+                ('Child Immunization / Vaccination', None, None),
+                ('Immunization or vaccination', None, None),
+                ('Tests', None, None),
+                ('Other\n(Including Antenatal Care)', None, None),
+            ]),
+            ('Feedback', [
+                ('Staff Treatment', '74%', 2),
+                ('Affordable Drugs', '89%', 3),
+                ('Avg. Wait Time', '1 hr.', 3),
+            ]),
         ],
     },
     'yashi-madaki-phc': {
