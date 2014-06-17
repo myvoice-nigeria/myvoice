af29a33b25016ee0b1186ed1a69a121d3a5e06ed
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-06-16 22:01:26 -0400

    Add ClinicStatistic inline to Clinic in admin

--- myvoice/clinics/admin.py
+++ myvoice/clinics/admin.py
@@ -8,8 +8,16 @@ class ClinicStaffInline(admin.TabularInline):
     extra = 0
 
 
+# NOTE: This is included for early development/debugging purposes. Eventually,
+# there will be many more statistics for a clinic than a simple inline can
+# handle.
+class ClinicStatisticInline(admin.TabularInline):
+    model = models.ClinicInline
+    extra = 0
+
+
 class ClinicAdmin(admin.ModelAdmin):
-    inlines = [ClinicStaffInline]
+    inlines = [ClinicStaffInline, ClinicStatisticInline]
     list_display = ['name', 'lga']
     readonly_fields = ['lga_rank', 'pbf_rank']
 
