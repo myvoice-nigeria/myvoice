fc0416cfcc2b245e28a5725d09068185b194fe6e
bayo <bayokrapht@gmail.com>
2014-06-23 09:48:38 +0100

    implemented helper functions for getting the links from the pages, drilling down from the dashboard-data page, also a started class for parsing the data from the clinic page to get the name, lga, region as well as the statistics from the page

--- myvoice/clinics/dashboard_scraper.py
+++ myvoice/clinics/dashboard_scraper.py
@@ -7,23 +7,108 @@ How Dashboard Scaper works:
 #TODO: How to find if the page has changed?
 '''
 import requests
-#from urllib2 import URLError
+import re
+from datetime import date
+from functools import partial
+from itertools import chain
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 
+# Does this go in the settings?
 DASHBOARD_URL = 'https://nphcda.thenewtechs.com/data.html'
-DIV_LINKS_CLASS = 'quality-b'
 
 
-def get_links():
-    '''Returns dict of clinic_name: url'''
-    try:
-        resp = requests.get(DASHBOARD_URL)
-    except requests.ConnectionError:
-        content = ''
-    else:
-        content = resp.content
+def get_soup(url, **kwargs):
+    '''
+    Get the soup of elements we are interested in
+    '''
+    elem = kwargs.get('elem', 'div')
+    attr = kwargs.get('attr', 'id')
+    value = kwargs.get('value', 'map_region')
+
+    resp = requests.get(url)
+    content = resp.content
     soup = BeautifulSoup(content)
-    div = soup.find('div', {'class': DIV_LINKS_CLASS})
-    elems = div.findAll('a')
-    links = {elem.get_text().strip(): elem.get('href') for elem in elems}
-    return links
+    return soup.find(elem, {attr: value})
+
+
+def get_links(url, **kwargs):
+    '''
+    Return links from content of url.
+
+    url: url to fetch
+    elem: parent element of links, defaults to 'div'
+    attr: the attribute to filter by, defaults to 'id'
+    value: the filter value, defaults to 'map_region'
+    '''
+    try:
+        parent = get_soup(url, **kwargs)
+    except requests.ConnectionError:
+        return {}
+    elems = parent.find_all('a')
+    return {elem.get_text().strip(): elem.get('href') for elem in elems}
+
+
+def get_clinic_links():
+    '''
+    Get links for each clinic.
+
+    We don't need corresponding name, lga and state(region?)
+    because we can get these from the page we are going to scrape.
+    '''
+    region_data = get_links(DASHBOARD_URL, elem='div', attr='id', value='map_region')
+    region_links = region_data.values()
+
+    lga_func = partial(get_links, elem='div', attr='id', value='map_region')
+    lga_pool = Pool()
+    lga_data = lga_pool.map(lga_func, region_links)
+    lga_links = chain(*[i.values() for i in lga_data])
+
+    clinic_func = partial(get_links, elem='ul', attr='class', value='links-a')
+    clinic_pool = Pool()
+    clinic_data = clinic_pool.map(clinic_func, lga_links)
+    clinic_links = chain(*[i.values() for i in clinic_data])
+    return clinic_links
+
+
+class ClinicPage(object):
+    LGA_PATT = re.compile('showentities')
+    REGION_PATT = re.compile('showzone')
+    HEADER_PATT = re.compile('[IV\d]+')
+
+    def __init__(self, soup):
+        self.soup = soup
+        self.nav_elem = self.extract_nav_elem()
+
+    def extract_nav_elem(self):
+        '''Extract the nav element that contains clinic, lga and region info
+        '''
+        return self.soup.find('nav', class_='crumbs-a')
+
+    def get_clinic_name(self):
+        return self.nav_elem.find('li', class_='active').text
+
+    def get_lga_name(self):
+        return self.nav_elem.find(href=self.LGA_PATT).text
+
+    def get_region_name(self):
+        return self.nav_elem.find(href=self.REGION_PATT).text
+
+    @classmethod
+    def get_month(self, header):
+        '''Converts header to date'''
+        roman_map = {
+            'I': 1,
+            'II': 4,
+            'III': 7,
+            'IV': 10
+        }
+        qtr, year = self.HEADER_PATT.findall(header)
+
+        try:
+            month = int(qtr)
+        except ValueError:
+            month = roman_map.get(qtr)
+
+        if month:
+            return date(int(year), month, 1)
--- myvoice/clinics/tests/test_scraper.py
+++ myvoice/clinics/tests/test_scraper.py
@@ -1,57 +1,177 @@
 import mock
+from datetime import date
+import re
+import requests
 
 from django.test import TestCase
 
 from .. import dashboard_scraper
 
-main_html = '''
-<div class="quality-b">
-<article class="item">
-<figure><img alt="No picture available for this entity" src="https://nphcda.thenewtechs.com/cside/images/portal/1Wayo_Matti.jpg"/></figure>
-<h4>Top score quality</h4>
-<p class="title"><span>1.</span><a href="https://nphcda.thenewtechs.com/data/showentity/5.html">Wayo Matti (HC) </a></p>
-<p class="global"><span class="badge-a up">97%</span></p>
-</article>
-<div class="table">
-<div class="head">
-<p class="title"><span>Name</span></p>
-<p class="global">Global</p>
-</div>
-<div class="item">
-<p class="title">5. <a href="https://nphcda.thenewtechs.com/data/showentity/2.html">Wamba (GH) </a></p>
-<p class="global"><i class="badge-b "></i> 76%</p>
-</div>
-</div>
+
+region_page = '''
+<div id="map_region">
+<ul class="links-a grid-a link-list-inline">
+<li class="column w100">
+<strong></strong>
+<ul class="list-unstyled link-list-inline">
+<li><a href="https://nphcda.thenewtechs.com/data/showzone/1/5.html" onmouseout="unhighlight('area5');" onmouseover="highlight('area5');">Adamawa</a></li>
+<li><a href="https://nphcda.thenewtechs.com/data/showzone/1/26.html" onmouseout="unhighlight('area26');" onmouseover="highlight('area26');">Nasarawa</a></li>
+<li><a href="https://nphcda.thenewtechs.com/data/showzone/1/29.html" onmouseout="unhighlight('area29');" onmouseover="highlight('area29');">Ondo</a></li>
+</ul>
+</li>
+</ul>
 </div>
 '''
 
 
-class TestScraper(TestCase):
+lga_page = '''
+<ul class="links-a grid-a">
+<li class="column w100">
+<strong></strong>
+<ul class="list-unstyled link-list-inline">
+<li>Health Center
+    <ul>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/187.html">ARDO YAHAYA (MBAMBA) HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/188.html">BAKARI MBAMOI HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/183.html">BAKO HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/192.html">GONGOSHI HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/185.html">LAMIDO ALIYU HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/189.html">NAMTARI MANGA HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/186.html">NANA ASMAU HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/190.html">NGURORE HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/184.html">SHAGARI HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/191.html">TOUNGO HC</a></li>
+    <li><a href="https://nphcda.thenewtechs.com/data/showentity/182.html">WURO-HAUSA HC</a></li>
+    </ul>
+    </li>
+    </ul>
+    </li>
+    </ul>
+'''
+
+
+nav_page = '''
+<nav class="crumbs-a">
+<ul>
+<li><a href="https://nphcda.thenewtechs.com/">Home</a></li>
+<li><a href="https://nphcda.thenewtechs.com/data">Data</a></li>
+<li><a href="https://nphcda.thenewtechs.com/data/showzone/1/26">Nasarawa</a></li>
+<li><a href="https://nphcda.thenewtechs.com/data/showentities/779">Wamba</a></li>
+<li class="active">Wamba</li>
+</ul>
+</nav>
+'''
+
+
+class TestGetLinks(TestCase):
+
+    def setUp(self):
+        self.mock_url = mock.Mock()
+        self.params = {'elem': 'div', 'attr': 'id', 'value': 'map_region'}
 
     @mock.patch('requests.get')
     def test_get_links_count(self, get_mock):
+        '''Make sure the correct number of links are extracted.'''
         response_mock = mock.MagicMock()
-        response_mock.content = main_html
+        response_mock.content = region_page
         get_mock.return_value = response_mock
-        link_dict = dashboard_scraper.get_links()
-        self.assertEqual(2, len(link_dict))
+        link_dict = dashboard_scraper.get_links(self.mock_url, **self.params)
+        self.assertEqual(3, len(link_dict))
 
     @mock.patch('requests.get')
-    def test_get_links_keys(self, get_mock):
+    def test_get_links_ul(self, get_mock):
+        '''Make sure we can get links in lists too.'''
         response_mock = mock.MagicMock()
-        response_mock.content = main_html
+        response_mock.content = lga_page
         get_mock.return_value = response_mock
-        link_dict = dashboard_scraper.get_links()
-        self.assertIn('Wayo Matti (HC)', link_dict)
-        self.assertIn('Wamba (GH)', link_dict)
+        params = {'elem': 'ul', 'attr': 'class', 'value': 'links-a'}
+        link_dict = dashboard_scraper.get_links(self.mock_url, **params)
+        self.assertEqual(11, len(link_dict))
+
+    @mock.patch('requests.get')
+    def test_get_region_links_keys(self, get_mock):
+        '''Make sure the keys of the links are correct.'''
+        response_mock = mock.MagicMock()
+        response_mock.content = region_page
+        get_mock.return_value = response_mock
+        link_dict = dashboard_scraper.get_links(self.mock_url, **self.params)
+        self.assertIn('Adamawa', link_dict)
+        self.assertIn('Nasarawa', link_dict)
+        self.assertIn('Ondo', link_dict)
 
     @mock.patch('requests.get')
     def test_get_links_values(self, get_mock):
-        href1 = 'https://nphcda.thenewtechs.com/data/showentity/2.html'
-        href2 = 'https://nphcda.thenewtechs.com/data/showentity/5.html'
+        '''Make sure the values of the links are correct.'''
+        href1 = 'https://nphcda.thenewtechs.com/data/showzone/1/5.html'
+        href2 = 'https://nphcda.thenewtechs.com/data/showzone/1/26.html'
+        href3 = 'https://nphcda.thenewtechs.com/data/showzone/1/29.html'
         response_mock = mock.MagicMock()
-        response_mock.content = main_html
+        response_mock.content = region_page
         get_mock.return_value = response_mock
-        link_dict = dashboard_scraper.get_links()
-        self.assertEqual(href2, link_dict.get('Wayo Matti (HC)'))
-        self.assertEqual(href1, link_dict.get('Wamba (GH)'))
+        link_dict = dashboard_scraper.get_links(self.mock_url, **self.params)
+        self.assertEqual(href1, link_dict.get('Adamawa'))
+        self.assertEqual(href2, link_dict.get('Nasarawa'))
+        self.assertEqual(href3, link_dict.get('Ondo'))
+
+    @mock.patch('requests.get')
+    def test_no_connection(self, get_mock):
+        '''If there is no connection, send an empty dictionary.'''
+        error = requests.ConnectionError()
+        get_mock.side_effect = error
+        link_dict = dashboard_scraper.get_links(self.mock_url, **self.params)
+        self.assertEqual({}, link_dict)
+
+
+class TestExtractPageData(TestCase):
+
+    def setUp(self):
+        self.soup_mock = mock.Mock()
+        self.elem_mock = mock.Mock()
+        self.soup_mock.find.return_value = self.elem_mock
+
+    def test_extract_clinic_name(self):
+        '''Scrape for the name of the clinic.'''
+        self.elem_mock.find.return_value.text = 'Wamba'
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        self.assertEqual('Wamba', page.get_clinic_name())
+        self.soup_mock.find.assert_called_once_with('nav', class_='crumbs-a')
+        self.elem_mock.find.assert_called_once_with('li', class_='active')
+
+    def test_extract_lga_name(self):
+        '''Extract the lga name from the page'''
+        patt = re.compile('showentities')
+        self.elem_mock.find.return_value.text = 'Wamba'
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        self.assertEqual('Wamba', page.get_lga_name())
+        self.soup_mock.find.assert_called_once_with('nav', class_='crumbs-a')
+        self.elem_mock.find.assert_called_once_with(href=patt)
+
+    def test_extract_region_name(self):
+        '''Extract the region (state) name from the page'''
+        patt = re.compile('showzone')
+        self.elem_mock.find.return_value.text = 'Nasarawa'
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        self.assertEqual('Nasarawa', page.get_region_name())
+        self.soup_mock.find.assert_called_once_with('nav', class_='crumbs-a')
+        self.elem_mock.find.assert_called_once_with(href=patt)
+
+    def test_get_month_from_header_roman(self):
+        '''The headers for the QTY, QLY and PAYMENT are irregular
+
+        Get the month from headers.
+        '''
+        month_1 = dashboard_scraper.ClinicPage.get_month('Q.I 2014')
+        month_2 = dashboard_scraper.ClinicPage.get_month('Q 4 2011')
+        month_3 = dashboard_scraper.ClinicPage.get_month('Qu. IV 2011')
+        self.assertEqual(date(2014, 1, 1), month_1)
+        self.assertEqual(date(2011, 4, 1), month_2)
+        self.assertEqual(date(2011, 10, 1), month_3)
+
+    def test_extract_payment_data(self):
+        '''Extract payment data from page
+
+        Payment data should be in dict with the quarter as key
+        and the amount as value.'''
+
+    def test_no_payment_data(self):
+        '''What happens when there is no data? Nothing I hope.'''
