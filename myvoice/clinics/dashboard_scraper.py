4b520f5c1bbe8d807d5115c85af7ce2a7131db8f
bayo <bayokrapht@gmail.com>
2014-06-24 11:42:32 +0100

    fleshed out ClinicPage class with methods for processing the page data + tests

--- myvoice/clinics/dashboard_scraper.py
+++ myvoice/clinics/dashboard_scraper.py
@@ -18,6 +18,10 @@ from bs4 import BeautifulSoup
 DASHBOARD_URL = 'https://nphcda.thenewtechs.com/data.html'
 
 
+class WrongHeaderError(Exception):
+    pass
+
+
 def get_soup(url, **kwargs):
     '''
     Get the soup of elements we are interested in
@@ -56,7 +60,8 @@ def get_clinic_links():
     We don't need corresponding name, lga and state(region?)
     because we can get these from the page we are going to scrape.
     '''
-    region_data = get_links(DASHBOARD_URL, elem='div', attr='id', value='map_region')
+    #region_data = get_links(DASHBOARD_URL, elem='div', attr='id', value='map_region')
+    region_data = get_links(DASHBOARD_URL)
     region_links = region_data.values()
 
     lga_func = partial(get_links, elem='div', attr='id', value='map_region')
@@ -72,27 +77,94 @@ def get_clinic_links():
 
 
 class ClinicPage(object):
+    # Regexes to search for lga or region links in page
     LGA_PATT = re.compile('showentities')
     REGION_PATT = re.compile('showzone')
+
+    # Regex to extract header information from table data
     HEADER_PATT = re.compile('[IV\d]+')
+
+    # css-class selectors for payment, quality, quantity
+    PAYMENT_SELECTOR = 'table-payment-a'
+    QUALITY_SELECTOR = 'table-qualities-a'
+    QUANTITY_SELECTOR = 'table-quantities-a'
 
     def __init__(self, soup):
         self.soup = soup
-        self.nav_elem = self.extract_nav_elem()
+        self._nav_elem = None
+        #self.nav_elem = self.extract_nav_elem()
 
-    def extract_nav_elem(self):
+    def get_nav_elem(self):
         '''Extract the nav element that contains clinic, lga and region info
         '''
+        if self._nav_elem:
+            return self._nav_elem
         return self.soup.find('nav', class_='crumbs-a')
 
     def get_clinic_name(self):
-        return self.nav_elem.find('li', class_='active').text
+        return self.get_nav_elem().find('li', class_='active').text
 
     def get_lga_name(self):
-        return self.nav_elem.find(href=self.LGA_PATT).text
+        return self.get_nav_elem().find(href=self.LGA_PATT).text
 
     def get_region_name(self):
-        return self.nav_elem.find(href=self.REGION_PATT).text
+        return self.get_nav_elem().find(href=self.REGION_PATT).text
+
+    def extract_table_content(self, div):
+        '''Extract body data from soup
+
+        Returns a list of lines
+        each line is a list of text data from table
+        '''
+        data = []
+        tr = div.find('tbody').find_all('tr')
+        for row in tr:
+            data.append([item.get_text() for item in row.find_all('td')])
+        return data
+
+    def extract_table_header(self, div):
+        '''Extract header data from soup
+
+        Returns a list of headers for the page section.
+        '''
+        th = div.find_all('th')
+        first = th[0].get_text()
+        rest = [self.get_month(item.get_text()) for item in th[1:]]
+        return [first] + rest
+
+    def find_table_div(self, selector_class):
+        return self.soup.find('div', class_=selector_class)
+
+    def merge_data(self, header, content):
+        '''Return a list of dicts for each line of content
+
+        The keys are the header.
+        '''
+        pass
+
+    def extract_table_data(self, selector_class):
+        '''Extract data from soup
+
+        Extracts data from each of payment, quality and quantity.'''
+        # Extract payment information
+        payment_div = self.find_table_div(self.PAYMENT_SELECTOR)
+        payment_header = self.extract_table_header(payment_div)
+        payment_content = self.extract_table_content(payment_div)
+        payment_data = self.merge_data(payment_header, payment_content)
+
+        # Extract quality information
+        qly_div = self.find_table_div(self.QUALITY_SELECTOR)
+        qly_header = self.extract_table_header(qly_div)
+        qly_content = self.extract_table_content(qly_div)
+        qly_data = self.merge_data(qly_header, qly_content)
+
+        # Extract quantity information
+        qty_div = self.find_table_div(self.QUANTITY_SELECTOR)
+        qty_header = self.extract_table_header(qty_div)
+        qty_content = self.extract_table_content(qty_div)
+        qty_data = self.merge_data(qty_header, qty_content)
+
+        return payment_data, qly_data, qty_data
 
     @classmethod
     def get_month(self, header):
@@ -103,7 +175,11 @@ class ClinicPage(object):
             'III': 7,
             'IV': 10
         }
-        qtr, year = self.HEADER_PATT.findall(header)
+        match = self.HEADER_PATT.findall(header)
+        if len(match) == 2:
+            qtr, year = match
+        else:
+            raise ValueError
 
         try:
             month = int(qtr)
--- myvoice/clinics/tests/test_scraper.py
+++ myvoice/clinics/tests/test_scraper.py
@@ -2,6 +2,7 @@ import mock
 from datetime import date
 import re
 import requests
+from functools import partial
 
 from django.test import TestCase
 
@@ -61,6 +62,18 @@ nav_page = '''
 </ul>
 </nav>
 '''
+
+
+def make_mocks(self, attr, values):
+    '''Utility fxn to create mocks and set return values
+
+    Returns a list of mocks.'''
+    mock_list = []
+    for value in values:
+        new_mock = mock.MagicMock()
+        setattr(new_mock, attr, value)
+        mock_list.append(new_mock)
+    return mock_list
 
 
 class TestGetLinks(TestCase):
@@ -155,7 +168,7 @@ class TestExtractPageData(TestCase):
         self.soup_mock.find.assert_called_once_with('nav', class_='crumbs-a')
         self.elem_mock.find.assert_called_once_with(href=patt)
 
-    def test_get_month_from_header_roman(self):
+    def test_get_month_from_header(self):
         '''The headers for the QTY, QLY and PAYMENT are irregular
 
         Get the month from headers.
@@ -167,11 +180,103 @@ class TestExtractPageData(TestCase):
         self.assertEqual(date(2011, 4, 1), month_2)
         self.assertEqual(date(2011, 10, 1), month_3)
 
-    def test_extract_payment_data(self):
-        '''Extract payment data from page
+    def test_get_month_wrong_data(self):
+        '''With wrong data should raise ValueError'''
+        self.assertRaises(
+            ValueError,
+            dashboard_scraper.ClinicPage.get_month, 'INDICATOR'
+        )
 
-        Payment data should be in dict with the quarter as key
-        and the amount as value.'''
 
-    def test_no_payment_data(self):
+class TestExtractTableData(TestCase):
+
+    def setUp(self):
+        self.header_mock1 = mock.Mock()
+        self.header_mock1.get_text.return_value = 'INDICATOR'
+        self.header_mock2 = mock.Mock()
+        self.header_mock2.get_text.return_value = 'Q.IV 2011'
+        self.header_mock3 = mock.Mock()
+        self.header_mock3.get_text.return_value = 'Q.I 2012'
+
+        self.td1 = mock.Mock()
+        self.td1.get_text.return_value = 'tag1'
+        self.td2 = mock.Mock()
+        self.td2.get_text.return_value = '224,989'
+        self.td3 = mock.Mock()
+        self.td3.get_text.return_value = '481,705'
+
+        self.td4 = mock.Mock()
+        self.td4.get_text.return_value = 'tag2'
+        self.td5 = mock.Mock()
+        self.td5.get_text.return_value = '727,850'
+        self.td6 = mock.Mock()
+        self.td6.get_text.return_value = '105,300'
+
+        # represents the soup of the whole page
+        self.soup_mock = mock.Mock()
+
+        # represents the particular element containing the data
+        self.elem_mock = mock.Mock()
+
+    def test_find_payment_elem(self):
+        '''We use proper selector to locate payment data'''
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        page.find_table_div(page.PAYMENT_SELECTOR)
+        self.soup_mock.find.assert_called_once_with('div', class_='table-payment-a')
+
+    def test_find_quality_elem(self):
+        '''We use proper selector to locate quality data'''
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        page.find_table_div(page.QUALITY_SELECTOR)
+        self.soup_mock.find.assert_called_once_with('div', class_='table-qualities-a')
+
+    def test_find_quantity_elem(self):
+        '''We use proper selector to locate quantity data'''
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        page.find_table_div(page.QUANTITY_SELECTOR)
+        self.soup_mock.find.assert_called_once_with('div', class_='table-quantities-a')
+
+    def test_extract_header(self):
+        '''Extract header data from soup
+
+        Return a list of headers
+        '''
+
+        header = [self.header_mock1, self.header_mock2, self.header_mock3]
+
+        self.elem_mock.find_all.return_value = header
+
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        data = page.extract_table_header(self.elem_mock)
+        self.assertEqual(3, len(data))
+        self.assertEqual(
+            ['INDICATOR', date(2011, 10, 1), date(2012, 1, 1)], data)
+
+    def test_extract_content(self):
+        '''Extract body data from soup
+
+        Returns a list of lines contained in the table.
+        '''
+        # <tr> elems contain multiple <td> elems each
+        tr1 = mock.Mock()
+        tr1.find_all.return_value = [self.td1, self.td2, self.td3]
+        tr2 = mock.Mock()
+        tr2.find_all.return_value = [self.td4, self.td5, self.td6]
+
+        # <tbody> elem contains multiple <tr> elems
+        tbody = mock.Mock()
+        tbody.find_all.return_value = [tr1, tr2]
+
+        # <div> elem contains <tbody> elem
+        div = mock.Mock()
+        div.find.return_value = tbody
+
+        page = dashboard_scraper.ClinicPage(self.soup_mock)
+        data = page.extract_table_content(div)
+        self.assertEqual(2, len(data))
+        row1 = ['tag1', '224,989', '481,705']
+        row2 = ['tag2', '727,850', '105,300']
+        self.assertEqual([row1, row2], data)
+
+    def _test_no_payment_data(self):
         '''What happens when there is no data? Nothing I hope.'''
