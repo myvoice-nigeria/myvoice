d30797eeff08a8b56b15451e96ce666ea84648cd
bayo <bayokrapht@gmail.com>
2014-07-10 04:08:37 +0100

    extract queryset for csv export

--- myvoice/core/tests/test_utils.py
+++ myvoice/core/tests/test_utils.py
@@ -1,4 +1,5 @@
 import datetime
+import mock
 
 from django.test import TestCase
 
@@ -29,3 +30,42 @@ class TestUtils(TestCase):
         """Test that it works for small percentages."""
         percentage = utils.make_percentage(1, 10000, 2)
         self.assertEqual(0.01, percentage)
+
+
+class TestCSVExport(TestCase):
+
+    def setUp(self):
+        self.qset = mock.MagicMock()
+        self.row1 = mock.Mock()
+        self.row1.name = 'me'
+        self.row1.address = 'somewhere'
+        self.row1.counter = 5
+
+        self.row2 = mock.Mock()
+        self.row2.name = 'you'
+        self.row2.address = 'elsewhere'
+        self.row2.counter = 17
+
+        self.qset.model._meta.fields.get_all_field_names.return_value = [
+            'name', 'address', 'counter']
+        #self.qset.side_effect = [self.row1, self.row2]
+
+        def side_effect():
+            for row in [self.row1, self.row2]:
+                yield row
+
+        self.qset.__iter__.side_effect = side_effect
+
+    def test_extract_qset_headers(self):
+        """Test that we can get the headers of the qset."""
+        data = utils.extract_qset_data(self.qset)
+        header = data[0]
+        self.assertEqual(['name', 'address', 'counter'], header)
+        self.assertTrue(self.qset.assert_called_once())
+
+    def test_extract_qset_data(self):
+        """Test that we can get the data from the qset."""
+        data = utils.extract_qset_data(self.qset)
+        self.assertEqual(['me', 'somewhere', '5'], data[1])
+        self.assertEqual(['you', 'elsewhere', '17'], data[2])
+        self.assertTrue(self.qset.assert_called_once())
--- myvoice/core/utils.py
+++ myvoice/core/utils.py
@@ -18,3 +18,15 @@ def make_percentage(numerator, denominator, places=0):
     """Returns a percentage out of 100 to the number of places given."""
     percentage = float(numerator) / float(denominator)
     return round(percentage * 100, places)
+
+
+def extract_qset_data(qset):
+    """Extract data from queryset for export to CSV file."""
+    fld_names = [fld for fld in qset.model._meta.fields.get_all_field_names()
+                 if fld not in ['id']]
+    out = [[i for i in fld_names]]
+    for obj in qset:
+        line = [str(getattr(obj, fld_name)).decode('utf-8', 'ignore')
+                for fld_name in fld_names]
+        out.append(line)
+    return out
