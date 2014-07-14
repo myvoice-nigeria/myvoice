576199d857d473b695b0402b1e2581442d3f1052
Richard Schwab <endura29@gmail.com>
2014-07-13 20:38:41 -0400

    added analysts directory

---
+++ myvoice/analysts/forms.py
@@ -0,0 +1,162 @@
+from django import forms
+from django.utils import timezone
+
+import json
+import re
+from datetime import timedelta
+
+from . import models
+
+
+VISIT_EXPR = '''
+^                               # Start
+\s*                             # Leading whitespace
+(i|I|\d)+                       # Numbers as clinic
+(\s|\*)+                        # Whitespace or '*'
+((1|i|I)|((i|I|o|O|[0-9])+))    # Either 1 or numbers as mobile
+(\s|\*)+                        # Whitespace or '*'
+(i|I|o|O|[0-9])+                # Numbers as serial
+(\s|\*)+                        # Whitespace or '*'
+(i|I|o|O|[0-9])+                # Numbers as Service
+\s*                             # Trailing whitespace
+$                               # End
+'''
+VISIT_PATT = re.compile(VISIT_EXPR, re.VERBOSE)
+
+
+class VisitForm(forms.Form):
+    phone = forms.CharField(max_length=20)
+    text = forms.RegexField(VISIT_PATT, max_length=200, error_messages={
+        'invalid': '1 or more parts of your entry are missing, please check and '
+        'enter the registration again.'})
+
+    serial_min = 3
+    serial_max = 6
+    min_wait_time = 1800  # Minimum time between visits by same patient in seconds
+
+    def __init__(self, *args, **kwargs):
+        super(VisitForm, self).__init__(*args, **kwargs)
+        self.error_list = []
+
+    def replace_alpha(self, text):
+        """Convert 'o' and 'O' to '0', and 'i', 'I' to '1'."""
+        return text.replace('o', '0').replace('O', '0').replace('i', '1').replace(
+            'I', '1').replace('*', ' ')
+
+    def clean_text(self):
+        """Validate input text.
+
+        text is in format: CLINIC MOBILE SERIAL SERVICE
+        """
+        # How come this is available here?
+        sender = self.cleaned_data['phone']
+
+        cleaned_data = self.replace_alpha(self.cleaned_data['text'].strip())
+        clnc, mobile, serial, srvc = cleaned_data.split()
+
+        # Check if mobile is correct
+        if len(mobile) not in [1, 11]:
+            self.error_list.append('mobile')
+
+        # Check if clinic is valid
+        try:
+            clinic = models.Clinic.objects.get(code=clnc)
+        except (models.Clinic.DoesNotExist, ValueError):
+            self.error_list.append('clinic')
+            clinic = None
+
+        # Check if serial is valid
+        if len(serial) < self.serial_min or len(serial) > self.serial_max:
+            self.error_list.append('serial')
+
+        # Check if Service is valid
+        try:
+            service = models.Service.objects.get(code=srvc)
+        except models.Service.DoesNotExist:
+            self.error_list.append('service')
+            service = None
+
+        # Check if there are errors.
+        # If 2 previous validation errors exist, allow entry to pass
+        # As long as mobile is ok.
+        if len(self.error_list) > 0:
+            fld_list = ', '.join(self.error_list).upper()
+            if models.VisitRegistrationError.objects.filter(
+                    sender=sender).count() >= 2 and 'mobile' not in self.error_list:
+                # Save error log
+                models.VisitRegistrationErrorLog.objects.create(
+                    sender=sender,
+                    error_type=fld_list,
+                    message=self.cleaned_data['text'])
+                # Clear Current Error state
+                models.VisitRegistrationError.objects.filter(
+                    sender=sender).delete()
+            else:
+                # Save error state
+                models.VisitRegistrationError.objects.create(sender=sender)
+                error_msg = 'Error for serial {0}. There was a mistake in entering '\
+                    '{1}. Please check and enter the whole registration '\
+                    'code again.'.format(serial, fld_list)
+                raise forms.ValidationError(error_msg)
+        else:  # No errors, check if a duplicate in 30 mins
+            min_wait_time = timezone.now() - timedelta(
+                seconds=self.min_wait_time)
+            if models.Visit.objects.filter(
+                    mobile=mobile,
+                    patient__serial=int(serial),  # FIXME: serial will change to text
+                    patient__clinic=clinic,
+                    visit_time__gt=min_wait_time).count():
+                raise forms.ValidationError("This registration was "
+                                            "received before. Thank you.")
+
+        return clinic, mobile, serial, service, self.cleaned_data['text']
+
+
+class SelectClinicForm(forms.Form):
+    clinic = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, *args, **kwargs):
+        super(SelectClinicForm, self).__init__(*args, **kwargs)
+        self.fields['clinic'].queryset = models.Clinic.objects.order_by('name')
+
+
+class FeedbackForm(forms.Form):
+    """
+    Requirements from TextIt Generic feedback flow
+    Clinic ID response come as numeric category with label "Clinic".
+    Clinic name (if clinic is not sent) comes as text with label "Which Clinic".
+    Complaint message comes as text with label "General Feedback".
+    Any message that comes with category "Other" is ignored.
+
+    More:
+    Clinics configured in Textit flow have corresponding code in Clinic model.
+    """
+    phone = forms.CharField(max_length=20)
+    values = forms.CharField()
+
+    def clean_values(self):
+        """Return Clinic and Message."""
+        data = self.cleaned_data['values'].replace('+', ' ')
+        values = json.loads(data)
+
+        clinic = None
+        clinic_text = ''
+        message = ''
+
+        for item in values:
+            category = item.get('category').lower()
+            label = item.get('label').lower()
+            value = item.get('value')
+
+            if category == 'other':
+                continue
+            elif label == 'general feedback':
+                message = value
+            elif label == 'clinic':
+                clinic = models.Clinic.objects.get(code=category)
+            elif label == 'which clinic':
+                clinic_text = value
+
+        if clinic_text:
+            message += ' ({})'.format(clinic_text)
+        return {'clinic': clinic, 'message': message}
---
+++ myvoice/analysts/models.py
@@ -0,0 +1,185 @@
+from django.contrib.gis.db import models as gis
+from django.db import models
+from django.utils import timezone
+
+from myvoice.core.validators import validate_year
+
+
+class Region(gis.Model):
+    """Geographical regions"""
+    TYPE_CHIOCES = (
+        ('country', 'Country'),
+        ('state', 'State'),
+        ('lga', 'LGA'),
+    )
+    name = models.CharField(max_length=255)
+    alternate_name = models.CharField(max_length=255, blank=True)
+    type = models.CharField(max_length=16, choices=TYPE_CHIOCES, default='lga')
+    external_id = models.IntegerField("External ID")
+    boundary = gis.MultiPolygonField()
+
+    objects = gis.GeoManager()
+
+    class Meta(object):
+        unique_together = ('external_id', 'type')
+
+    def __unicode__(self):
+        return "{} - {}".format(self.get_type_display(), self.name)
+
+
+class Clinic(models.Model):
+    """A health clinic."""
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    # These might later become location-based models. LGA should be
+    # easily accessible (not multiple foreign keys away) since it is the
+    # designator that we are most interested in.
+    town = models.CharField(max_length=100)
+    ward = models.CharField(max_length=100)
+    lga = models.CharField(max_length=100, verbose_name='LGA')
+    location = gis.PointField(null=True, blank=True)
+
+    lga_rank = models.IntegerField(
+        blank=True, null=True, verbose_name='LGA rank', editable=False)
+    pbf_rank = models.IntegerField(
+        blank=True, null=True, verbose_name='PBF rank', editable=False)
+
+    code = models.PositiveIntegerField(
+        verbose_name='SMS Code', unique=True,
+        help_text="Code of Clinic to be used in SMS registration.")
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = gis.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+    def managers(self):
+        """The staff members who are in charge of this clinic."""
+        return self.clinicstaff_set.filter(is_manager=True)
+
+
+class ClinicStaff(models.Model):
+    """Represents a person who works at a Clinic."""
+    clinic = models.ForeignKey('Clinic')
+
+    user = models.ForeignKey(
+        'auth.User', blank=True, null=True,
+        help_text="If possible, this person should have a User account.")
+    name = models.CharField(
+        max_length=100, blank=True,
+        help_text="If given, the User account's name will be preferred to the "
+        "name given here with the assumption that it is more likely to be "
+        "current.")
+    contact = models.ForeignKey(
+        'rapidsms.Contact', verbose_name='Preferred contact', blank=True, null=True,
+        help_text="If not given but a User is associated with this person, "
+        "the User's first associated Contact may be used.")
+
+    # It would be nice to make this a choice field if we could get a list
+    # of all possible staff position types.
+    staff_type = models.CharField(max_length=100)
+
+    year_started = models.CharField(
+        max_length=4, blank=True, validators=[validate_year],
+        help_text="Please enter a four-digit year.")
+    is_manager = models.BooleanField(
+        default=False,
+        help_text="Whether this person is considered in charge of the clinic.")
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.get_name_display()
+
+    def get_name_display(self):
+        """Prefer the associated User's name to the name specified here."""
+        return self.user.get_full_name() if self.user else self.name
+
+
+class Service(models.Model):
+    """A medical service offered by a Clinic."""
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+    code = models.PositiveIntegerField(
+        verbose_name='SMS Code', unique=True,
+        help_text="Code of Service to be used in SMS registration.")
+
+    def __unicode__(self):
+        return self.name
+
+
+class Patient(models.Model):
+    """Represents a patient at the Clinic."""
+    name = models.CharField(max_length=50, blank=True)
+    clinic = models.ForeignKey('Clinic', blank=True, null=True)
+    mobile = models.CharField(max_length=11, blank=True)
+    serial = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = [('clinic', 'serial')]
+
+    def __unicode__(self):
+        return '{0} at {1}'.format(self.serial, self.clinic.name)
+
+
+class Visit(models.Model):
+    """Represents a visit of a Patient to the Clinic."""
+    patient = models.ForeignKey('Patient')
+    service = models.ForeignKey('Service', blank=True, null=True)
+    staff = models.ForeignKey('ClinicStaff', blank=True, null=True)
+    visit_time = models.DateTimeField(default=timezone.now)
+    welcome_sent = models.DateTimeField(blank=True, null=True)
+    survey_sent = models.DateTimeField(blank=True, null=True)
+    mobile = models.CharField(max_length=11, blank=True)
+
+    def __unicode__(self):
+        return unicode(self.patient)
+
+
+class VisitRegistrationError(models.Model):
+    """Keeps current state of errors in Visit registration SMS.
+
+    Right now, only "wrong clinic" is useful."""
+
+    WRONG_CLINIC = 0
+    WRONG_MOBILE = 1
+    WRONG_SERIAL = 2
+    WRONG_SERVICE = 3
+
+    ERROR_TYPES = enumerate(('Wrong Clinic', 'Wrong Mobile', 'Wrong Serial', 'Wrong Service'))
+
+    sender = models.CharField(max_length=20)
+    error_type = models.PositiveIntegerField(choices=ERROR_TYPES, default=WRONG_CLINIC)
+
+    def __unicode__(self):
+        return self.sender
+
+
+class VisitRegistrationErrorLog(models.Model):
+    """Keeps log of errors in Visit registration SMS."""
+    sender = models.CharField(max_length=20)
+    error_type = models.CharField(max_length=50)
+    message_date = models.DateTimeField(auto_now=True)
+    message = models.CharField(max_length=160)
+
+    def __unicode__(self):
+        return self.sender
+
+
+class GenericFeedback(models.Model):
+    """Keeps Feedback information sent by patients."""
+    sender = models.CharField(max_length=20)
+    clinic = models.ForeignKey('Clinic', null=True, blank=True)
+    message = models.CharField(max_length=200, blank=True)
+    message_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Generic Feedback'
+
+    def __unicode__(self):
+        return self.sender
