b4b34a475320316e0e3d994cba90e759cbd146da
Tobias McNulty <tobias@caktusgroup.com>
2014-07-22 18:01:32 -0400

    correct DB user & name in local vagrant settings

--- myvoice/settings/local_vagrant.py
+++ myvoice/settings/local_vagrant.py
@@ -3,11 +3,11 @@ from myvoice.settings.staging import *  # noqa
 
 # There should be only minor differences from staging
 
-DATABASES['default']['NAME'] = 'myvoice_local'
-DATABASES['default']['USER'] = 'myvoice_local'
+DATABASES['default']['NAME'] = 'myvoice_local_vagrant'
+DATABASES['default']['USER'] = 'myvoice_local_vagrant'
 
-EMAIL_SUBJECT_PREFIX = '[Myvoice Local] '
+EMAIL_SUBJECT_PREFIX = '[Myvoice Local Vagrant] '
 
 # Uncomment if using celery worker configuration
-BROKER_URL = ('amqp://myvoice_local:'
-              '%(BROKER_PASSWORD)s@%(BROKER_HOST)s/myvoice_local' % os.environ)
+BROKER_URL = ('amqp://myvoice_local_vagrant:'
+              '%(BROKER_PASSWORD)s@%(BROKER_HOST)s/myvoice_local_vagrant' % os.environ)
