b9e55ef97e4ca217da681a6a455741640dbbde89
bayo <bayokrapht@gmail.com>
2014-07-25 17:30:27 +0100

    added tests for analyze and get_mode

--- myvoice/survey/tests/test_utils.py
+++ myvoice/survey/tests/test_utils.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from .. import utils
 
+from myvoice.core.tests import factories
+
 
 class TestDisplayFeedback(TestCase):
 
@@ -15,3 +17,25 @@ class TestDisplayFeedback(TestCase):
         good_feedback = ['Yes this is good', 'Great feedback', '20']
         for good in good_feedback:
             self.assertEqual(utils.display_feedback(good), True)
+
+
+class TestSurveyUtils(TestCase):
+
+    def setUp(self):
+        self.survey = factories.Survey.create()
+        self.question = factories.SurveyQuestion.create(survey=self.survey)
+        self.responses = [factories.SurveyQuestionResponse.create(
+            response=ans, question=self.question)
+            for ans in ('Yes', 'Yes', 'Yes', 'No')]
+
+    def test_analyze(self):
+        """Test it returns percentage of responses with a given answer."""
+        self.assertEqual(75, utils.analyze(self.responses, 'Yes'))
+        self.assertEqual(25, utils.analyze(self.responses, 'No'))
+
+    def test_get_mode(self):
+        "Test that get_mode function finds the most common item."""
+        for i in range(3):
+            self.responses.append(factories.SurveyQuestionResponse.create(
+                response='No', question=self.question))
+        self.assertEqual('No', utils.get_mode(self.responses))
