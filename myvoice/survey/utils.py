05c52257427002cf6b4802f713977365b4db4b6e
bayo <bayokrapht@gmail.com>
2014-07-31 09:26:43 +0100

    added start to list of feedback not to be displayed

--- myvoice/survey/utils.py
+++ myvoice/survey/utils.py
@@ -101,6 +101,6 @@ def display_feedback(response_text):
     """Returns whether or not the text response should be displayed."""
     if not response_text or len(response_text.strip()) <= 1:
         return False
-    if response_text.strip().lower() in ['55999', 'yes', 'no', 'n0']:
+    if response_text.strip().lower() in ['55999', 'yes', 'no', 'n0', 'start']:
         return False
     return True
