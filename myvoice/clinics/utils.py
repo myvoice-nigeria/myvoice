99140d9e5b10119b39670efe89eb634a8cf39246
elo80ka <stan.madueke@gmail.com>
2015-01-07 11:56:27 +0100

    Added utility function to group clinics by State and LGA.

--- myvoice/clinics/utils.py
+++ myvoice/clinics/utils.py
@@ -1,5 +1,15 @@
 from myvoice.clinics.models import Visit
+from itertools import groupby
 
+def group_facilities(clinics):
+    """
+    Groups an iterable of Clinics, first by State, then by LGA.
+    
+    Note that we assume the list is already sorted.
+    """
+    by_lga = groupby(clinics, lambda c: c.lga)
+    by_state = groupby(by_lga, lambda g: g[0].state)
+    return by_state
 
 def get_triggered_count(a_clinic="", service="", start_date="", end_date=""):
     st_query = Visit.objects.filter(survey_sent__isnull=False)
