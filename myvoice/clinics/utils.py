dc6574b6d209971e324d56062f0c7a527a050c3a
elo80ka <stan.madueke@gmail.com>
2015-01-07 15:11:00 +0100

    Fixed 'group_facilities' method, so it returned more than one item.

--- myvoice/clinics/utils.py
+++ myvoice/clinics/utils.py
@@ -1,14 +1,17 @@
 from myvoice.clinics.models import Visit
 from itertools import groupby
 
-def group_facilities(clinics):
+def groupbylist(*args, **kwargs):
+    return [(k, list(g)) for k, g in groupby(*args, **kwargs)]
+
+def group_facilities(facilities):
     """
-    Groups an iterable of Clinics, first by State, then by LGA.
+    Groups an iterable of Facilities, first by State, then by LGA.
     
     Note that we assume the list is already sorted.
     """
-    by_lga = groupby(clinics, lambda c: c.lga)
-    by_state = groupby(by_lga, lambda g: g[0].state)
+    by_lga = groupbylist(facilities, lambda x: x.lga)
+    by_state = groupbylist(by_lga, lambda x: x[0].state)
     return by_state
 
 def get_triggered_count(a_clinic="", service="", start_date="", end_date=""):
