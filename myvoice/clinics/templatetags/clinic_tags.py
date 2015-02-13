6a8f3fe14aaa01d9246fe31f8eff9f1cd77f3227
elo80ka <stan.madueke@gmail.com>
2015-02-13 08:38:07 +0100

    Modified facility graph to highlight the current facility.

---
+++ myvoice/clinics/templatetags/clinic_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_index(list, value):
+    """Returns the index of `value` in the specified `list`."""
+    try:
+        value = '\n'.join(value.split())
+        return list.index(value)
+    except ValueError:
+        return -1
